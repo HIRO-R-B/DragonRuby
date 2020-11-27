4acdda03b42ed6827ebc406f057c1545539776d0
HIRO-R-B <35461694+HIRO-R-B@users.noreply.github.com>
2020-11-27 08:29:23 -0800

    Add files via upload

---
+++ asset_browser.rb
@@ -0,0 +1,499 @@
+return if $gtk.production
+
+require 'lib/dimensions.rb'
+
+module GTK
+  class AssetBrowser
+    class Label
+      attr :x, :y, :text,
+           :size_enum, :alignment_enum,
+           :r, :g, :b, :a,
+           :font
+
+      def initialize x:, y:, text:, se: 0, ae: 0, r: 255, g: 255, b: 255
+        @x = x
+        @y = y
+        @text = text
+        @size_enum = se
+        @alignment_enum = ae
+        @r = r
+        @g = g
+        @b = b
+      end
+
+      def primitive_marker
+        return :label
+      end
+    end
+
+    class Sprite
+      attr_sprite
+      attr :bw, :bh
+
+      def initialize
+        @x = 0
+        @y = 0
+        @w = 0
+        @h = 0
+        @path = ''
+
+        @bx = 640
+        @by = 360
+        @bw = 0
+        @bh = 0
+
+        move
+      end
+
+      def set_base_wh w, h
+        @bw = w
+        @bh = h
+      end
+
+      def set_path path
+        @path = path
+        @bw, @bh = getdimensions path
+      end
+
+      def shift point
+        @x = @bx + point.x - @w.half
+        @y = @by + point.y - @h.half
+      end
+
+      def set_shift point
+        @bx += point.x
+        @by += point.y
+        move
+      end
+
+      def move point = [@bx, @by]
+        @bx, @by = point.x, point.y
+        @x = @bx - @w.half
+        @y = @by - @h.half
+      end
+
+      def resize zoom
+        @w = zoom * @bw
+        @h = zoom * @bh
+      end
+    end
+
+    def ls path
+      return `ls \"#{path}\"`.split("\n")
+    end
+
+    def rec_ls base_path, path = ''
+      return ls("#{base_path}/#{path}").map do |f|
+        file = "#{base_path}/#{path}#{f}"
+        next rec_ls(base_path, "#{path}#{f}/") if File.directory? file
+        "#{path}#{f}" # if f.end_with? '.png'
+      end.reject { |v| v.nil? || v.empty?}
+    end
+
+    def generate_listing base_path, path, file_types, hash = {}
+      listing = ls("#{base_path}/#{path}")
+      return nil unless listing
+      dirs  = listing.select { |f| File.directory? "#{base_path}/#{path}#{f}" }
+      files = listing.select { |f| file_types.any? { |type| f.end_with? type } }
+      return nil if dirs.empty? && files.empty?
+
+      hash[path] = {}
+      hash[path][:dirs]  = dirs unless dirs.empty?
+      hash[path][:files] = files unless files.empty?
+      return hash unless hash[path][:dirs]
+      hash[path][:dirs].each_with_index do |f, i|
+        v = generate_listing base_path, "#{path}#{f}/", file_types, hash
+        hash[path][:dirs][i] = nil if v.nil?
+      end
+      hash[path][:dirs].reject! { |v| v.nil? }
+
+      return hash
+    end
+
+    def flat_list base_path, hash
+      return nil unless hash
+      list = __flat_list base_path, hash
+      return list.flatten
+    end
+
+    def __flat_list base_path, hash, array = [], path = ''
+      array << hash[base_path][:files].map { |f| "#{path}#{f}" } if hash[base_path][:files]
+      hash[base_path][:dirs].each do |dir|
+        __flat_list "#{base_path}#{dir}/", hash, array, "#{path}#{dir}/"
+      end if hash[base_path][:dirs]
+      return array
+    end
+
+    def wrapped_lines strings, joiner
+      lines = []
+      s = nil
+      strings.each do |str|
+        next s = str if s.nil?
+        v = [s, str].join(joiner)
+        if v.length < 129
+          s = v
+        else
+          lines << s
+          s = str
+        end
+      end
+      lines << s if !s.nil?
+      return lines
+    end
+
+    def pp obj, ind = ''
+      case obj
+      when Hash
+        puts ind + '{'
+        obj.map do |h, v|
+          pp h, ind + '  '
+          pp v, ind + '    '
+        end
+        puts ind + '}'
+      when Array
+        puts ind + '['
+        obj.map do |v|
+          pp v, ind + '  '
+        end
+        puts ind + ']'
+      when NilClass
+        puts "#{ind}NIL"
+      when String
+        puts obj.quote
+      else
+        puts "#{ind}#{obj}"
+      end
+    end
+
+    def initialize
+      @visible = false
+
+      @asset_modes   = [:sprites, :sounds].cycle
+      @asset_mode    = @asset_modes.next
+
+      @view_modes = [:dir, :listing].cycle
+      @view_mode  = @view_modes.next
+
+      @dir = $gtk.get_game_dir
+
+      @sprite_files = generate_listing @dir, "sprites/", ['.png']
+      @sound_files  = generate_listing @dir, "sounds/",  ['.wav', '.ogg']
+
+      @bwd = 'sprites/'
+      @cwd = @bwd
+
+      @files = @sprite_files
+      @file_listing = flat_list(@cwd, @files)
+
+      @idx = 0
+      @zoom = 1
+
+      @choice_chars = '1234567890qrtyuiopafghjklzxcvbnm'
+      @cmd = ''
+
+      @bg_solid   = { x: 0, y: 0, w: 1280, h: 720, r: 0, g: 0, b: 0, a: 224 }.solid
+      @title      = Label.new x: 1280, y: 720, text: title_text, ae: 2
+      @title_line = { x: 0, y: 698, x2: 1280, y2: 698, r: 255, g: 255, b: 255 }.line
+      @cwd_label  = Label.new x: 0, y: 720, text: cwd_text
+      @dir_labels = make_dir_labels
+      @dir_lines  = make_dir_lines
+      @sprite     = Sprite.new
+
+      return unless @files
+      set_sprite_path @cwd + @files[@cwd][:files][0] if @files[@cwd][:files]
+      @sprite.resize @zoom
+      @sprite.move
+
+      @cur_file = "#{@cwd}#{@files[@cwd][:files][@idx]}" if @files[@cwd][:files]
+    end
+
+    def title_text
+      return "#{@cmd.length > 0 ? '| CMD: ' + @cmd : '' } | #{@zoom} ZOOM | #{@view_mode.upcase} VIEW | #{@asset_mode.upcase} MODE | ASSET BROWSER "
+    end
+
+    def cwd_text
+      return 'DIR: NO DIRECTORY' unless @files
+      return "DIR: #{@cwd}"
+    end
+
+    def make_dir_labels
+      return [] unless @files&.dig @cwd, :dirs
+      lines = wrapped_lines @files[@cwd][:dirs].map.with_index { |dir, i| "[#{@choice_chars[i]}] #{dir}" }, ' | '
+      return lines.map.with_index { |line, i| Label.new x: 0, y: 698 - 22*i, text: line }
+    end
+
+    def make_dir_lines
+      return @dir_labels.length.times.map { |i| { x: 0, y: 676 - 22*i, x2: 1280, y2: 676 - 22*i, r: 255, g: 255, b: 255 }.line }
+    end
+
+    def toggle
+      @visible = !@visible
+      return
+    end
+
+    def toggle_asset_mode
+      @asset_mode = @asset_modes.next
+      @bwd = "#{@asset_mode}/"
+      @cwd = @bwd
+
+      case @asset_mode
+      when :sprites
+        @files = @sprite_files
+      when :sounds
+        @files = @sound_files
+      end
+
+      refresh
+      return
+    end
+
+    def toggle_view_mode
+      @view_mode = @view_modes.next
+      refresh
+      return
+    end
+
+    def refresh
+      @cwd_label.text = cwd_text
+      @title.text = title_text
+      @idx = 0
+
+      case @view_mode
+      when :dir
+        @dir_labels = make_dir_labels
+        @dir_lines  = make_dir_lines
+      else
+        @file_listing = flat_list(@cwd, @files)
+      end
+
+      return unless @files
+
+      case @asset_mode
+      when :sprites
+        case @view_mode
+        when :dir
+          set_sprite_path @files[@cwd][:files] ? "#{@cwd}#{@files[@cwd][:files][0]}" : nil
+          @cur_file = "#{@cwd}#{@files[@cwd][:files][@idx]}" if @files[@cwd][:files]
+        else
+          set_sprite_path "#{@cwd}#{@file_listing[@idx]}"
+          @cur_file = "#{@cwd}#{@file_listing[@idx]}"
+        end
+
+        @sprite.resize @zoom
+        @sprite.move
+      when :sounds
+      end
+
+      return
+    end
+
+    def getdimensions path
+      return Dimensions.dimensions "#{@dir}/#{path}"
+    end
+
+    def set_sprite_path path
+      @sprite.path = path
+      return @sprite.set_base_wh(0, 0) unless path
+      @sprite.set_base_wh(*getdimensions(path))
+    end
+
+    def sprite_mode_inputs inputs
+      kb = inputs.keyboard
+      kd = kb.key_down
+
+      if @cmd.length > 0
+        return (@cmd = ''; refresh) if kd.escape
+        @cmd += inputs.text[0] if inputs.text.length > 0
+        case @cmd[0]
+        when 'f'
+          if @cmd[1]
+            i = @choice_chars.index(@cmd[1])
+            @cwd += @files[@cwd][:dirs][i] + '/' if i && @files[@cwd][:dirs][i]
+            @cmd = ''
+            refresh
+          end
+        end
+        return
+      end
+
+      pdx = @idx
+      @idx -= kb.up_down
+      @idx -= 1 if kd.e
+      @idx += 1 if kd.d
+
+      case @view_mode
+      when :dir
+        if kd.r && @cwd != @bwd
+          @cwd = @cwd.split('/')[0..-2].join('/') + '/'
+          refresh
+        end
+
+        if kd.f && @cmd.length == 0 && @files[@cwd][:dirs]
+          @cmd = 'f'
+          refresh
+        end
+
+        @idx = @files[@cwd][:files] ? @idx % @files[@cwd][:files].length : 0
+
+        if pdx != @idx
+          set_sprite_path "#{@cwd}#{@files[@cwd][:files][@idx]}" if @files[@cwd][:files]
+          @sprite.resize @zoom
+          @sprite.move
+
+          @cur_file = "#{@cwd}#{@files[@cwd][:files][@idx]}" if @files[@cwd][:files]
+        end
+      else
+        if kd.f
+          paths = @file_listing[@idx].split('/')
+          if paths.length > 1
+            @cwd += paths[0] + '/'
+            refresh
+          end
+        end
+
+        if kd.r && @cwd != @bwd
+          @cwd = @cwd.split('/')[0..-2].join('/') + '/'
+          refresh
+        end
+
+        @idx = @idx % @file_listing.length
+        if pdx != @idx
+          set_sprite_path "#{@cwd}#{@file_listing[@idx]}"
+          @sprite.resize @zoom
+          @sprite.move
+
+          @cur_file = "#{@cwd}#{@file_listing[@idx]}"
+        end
+      end
+
+      m = inputs.mouse
+      pzoom = @zoom
+      @zoom = (m.wheel.y < 0 ? @zoom / 2 : @zoom * 2).clamp(0.125, 16) if m.wheel
+
+      if m.button_middle
+        @zoom = 1
+        @sprite.move [640, 360]
+      end
+
+      if pzoom != @zoom
+        @title.text = title_text
+        @sprite.resize @zoom
+        @sprite.move
+      end
+
+      @sprite.move m.point if m.button_left
+
+      if m.button_right
+        @m_grab ||= m.click
+        p2 = m.point
+        @sprite.shift [p2.x - @m_grab.x, p2.y - @m_grab.y]
+      else
+        if @m_grab
+          p2 = m.point
+          @sprite.set_shift [p2.x - @m_grab.x, p2.y - @m_grab.y]
+          @m_grab = nil
+        end
+      end
+    end
+
+    def inputs inputs
+      return unless $console.hidden?
+      toggle if inputs.keyboard.key_down.exclamation_point
+
+      return unless @visible
+      kb = inputs.keyboard
+      kd = kb.key_down
+
+      # toggle_asset_mode if kd.m # TODO: Add a sound mode
+      toggle_view_mode  if kd.n
+
+      return unless @files
+      case @asset_mode
+      when :sprites
+        sprite_mode_inputs inputs
+      when :sounds
+      end
+
+      $gtk.set_clipboard(@cur_file) if inputs.keyboard.key_down.enter && @cur_file
+    end
+
+    def render args
+      return unless @visible
+
+      case @asset_mode
+      when :sprites
+        case @view_mode
+        when :dir
+          if @files&.dig @cwd, :files
+            list = (@idx..@idx+29).map.with_index do |v, i|
+              s = @files[@cwd][:files][@idx - 1 + i]
+              s = v == 0 ? '' : '[...]' if i == 0
+              s = s ? '[...]' : '' if i == 29
+              Label.new x: 0, y: 690 - 22*@dir_lines.length - 22*i, text: s
+            end
+            list[1].g = 0
+            list[1].b = 0
+          else
+            list = []
+          end
+          list.push(*@dir_labels, *@dir_lines)
+        else
+          if @file_listing
+            list = (@idx..@idx+30).map.with_index do |v, i|
+              s = @file_listing[@idx - 1 + i]
+              s = v == 0 ? '' : '[...]' if i == 0
+              s = s ? '[...]' : '' if i == 30
+              Label.new x: 0, y: 690 - 22*i, text: s
+            end
+            list[1].g = 0
+            list[1].b = 0
+          else
+            list = []
+          end
+        end
+
+        args.outputs.reserved.unshift(
+          @bg_solid,
+          @title,
+          @title_line,
+          @cwd_label,
+          *list,
+          @sprite
+        )
+      when :sounds
+        list = []
+        args.outputs.reserved.unshift(
+          @bg_solid,
+          @title,
+          @title_line,
+          @cwd_label,
+          *list
+        )
+      end
+    end
+  end
+
+  module AssetBrowserExt
+    def assetbrowser
+      return @assetbrowser ||= AssetBrowser.new
+    end
+
+    def set_clipboard text
+      IO.popen("echo -n \"#{text}\" | xclip -sel c").close if self.platform == 'Linux'
+      `cmd /c \"echo | set /p dummy=#{text}| clip\"` if self.platform == 'Windows'
+      `echo -n \"#{text}\" | pbcopy` if self.platform == 'Max Os X'
+      return
+    end
+
+    def tick count
+      assetbrowser.inputs $args.inputs
+      super
+      assetbrowser.render $args
+      return
+    end
+  end
+
+  class Runtime
+    prepend AssetBrowserExt
+  end
+end
---
+++ dimensions.rb
@@ -0,0 +1,414 @@
+module Dimensions
+  class Scanner
+    class ScanError < ::StandardError; end
+
+    attr_reader :pos
+
+    def initialize(data)
+      @data = data.dup
+      @data.force_encoding("BINARY") if @data.respond_to?(:force_encoding)
+      @size = @data.length
+      @pos  = 0
+      big!  # endianness
+    end
+
+    def read_char
+      read(1, "C")
+    end
+
+    def read_short
+      read(2, @big ? "n" : "v")
+    end
+
+    def read_long
+      read(4, @big ? "N" : "V")
+    end
+
+    def read(size, format)
+      data = read_data(size)
+      data.unpack(format)[0]
+    end
+
+    def read_data(size)
+      data = @data[@pos, size]
+      advance(size)
+      data
+    end
+
+    def advance(length)
+      @pos += length
+      raise_scan_error if @pos > @size
+    end
+
+    def skip_to(pos)
+      @pos = pos
+      raise_scan_error if @pos > @size
+    end
+
+    def big!
+      @big = true
+    end
+
+    def little!
+      @big = false
+    end
+
+    def raise_scan_error
+      raise ScanError
+    end
+  end
+end
+
+module Dimensions
+  module TiffScanning
+    def scan_header
+      scan_endianness
+      scan_tag_mark
+      scan_and_skip_to_offset
+    end
+
+    def scan_endianness
+      tag = [read_char, read_char]
+      tag == [0x4D, 0x4D] ? big! : little!
+    end
+
+    def scan_tag_mark
+      raise_scan_error unless read_short == 0x002A
+    end
+
+    def scan_and_skip_to_offset
+      offset = read_long
+      skip_to(offset)
+    end
+
+    def scan_ifd
+      offset = pos
+      entry_count = read_short
+
+      entry_count.times do |i|
+        skip_to(offset + 2 + (12 * i))
+        tag = read_short
+        yield tag
+      end
+    end
+
+    def read_integer_value
+      type = read_short
+      advance(4)
+
+      if type == 3
+        read_short
+      elsif type == 4
+        read_long
+      else
+        raise_scan_error
+      end
+    end
+  end
+end
+
+module Dimensions
+  class JpegScanner < Scanner
+    SOF_MARKERS = [0xC0..0xC3, 0xC5..0xC7, 0xC9..0xCB, 0xCD..0xCF]
+    EOI_MARKER  = 0xD9  # end of image
+    SOS_MARKER  = 0xDA  # start of stream
+    APP1_MARKER = 0xE1  # maybe EXIF
+
+    attr_reader :width, :height, :angle
+
+    def initialize(data)
+      @width  = nil
+      @height = nil
+      @angle  = 0
+      super
+    end
+
+    def scan
+      advance(2)
+
+      while marker = read_next_marker
+        case marker
+        when *SOF_MARKERS
+          scan_start_of_frame
+        when EOI_MARKER, SOS_MARKER
+          break
+        when APP1_MARKER
+          scan_app1_frame
+        else
+          skip_frame
+        end
+      end
+
+      width && height
+    end
+
+    def read_next_marker
+      c = read_char while c != 0xFF
+      c = read_char while c == 0xFF
+      c
+    end
+
+    def scan_start_of_frame
+      length = read_short
+      read_char # depth, unused
+      height = read_short
+      width  = read_short
+      size   = read_char
+
+      if length == (size * 3) + 8
+        @width, @height = width, height
+      else
+        raise_scan_error
+      end
+    end
+
+    def scan_app1_frame
+      frame = read_frame
+      if frame[0..5] == "Exif\000\000"
+        scanner = ExifScanner.new(frame[6..-1])
+        if scanner.scan
+          case scanner.orientation
+          when :bottom_right
+            @angle = 180
+          when :left_top, :right_top
+            @angle = 90
+          when :right_bottom, :left_bottom
+            @angle = 270
+          end
+        end
+      end
+    rescue ExifScanner::ScanError
+    end
+
+    def read_frame
+      length = read_short - 2
+      read_data(length)
+    end
+
+    def skip_frame
+      length = read_short - 2
+      advance(length)
+    end
+  end
+end
+
+module Dimensions
+  class Reader
+    GIF_HEADER    = [0x47, 0x49, 0x46, 0x38]
+    PNG_HEADER    = [0x89, 0x50, 0x4E, 0x47]
+    JPEG_HEADER   = [0xFF, 0xD8, 0xFF]
+    TIFF_HEADER_I = [0x49, 0x49, 0x2A, 0x00]
+    TIFF_HEADER_M = [0x4D, 0x4D, 0x00, 0x2A]
+
+    attr_reader :type, :width, :height, :angle
+
+    def initialize
+      @process = :determine_type
+      @type    = nil
+      @width   = nil
+      @height  = nil
+      @angle   = nil
+      @size    = 0
+      @data    = ""
+      @data.force_encoding("BINARY") if @data.respond_to?(:force_encoding)
+    end
+
+    def <<(data)
+      if @process
+        @data << data
+        @size = @data.length
+        process
+      end
+    end
+
+    def process(process = @process)
+      send(@process) if @process = process
+    end
+
+    def determine_type
+      if @size >= 4
+        bytes = @data.unpack("C4")
+
+        if match_header(GIF_HEADER, bytes)
+          @type = :gif
+        elsif match_header(PNG_HEADER, bytes)
+          @type = :png
+        elsif match_header(JPEG_HEADER, bytes)
+          @type = :jpeg
+        elsif match_header(TIFF_HEADER_I, bytes) || match_header(TIFF_HEADER_M, bytes)
+          @type = :tiff
+        end
+
+        process @type ? :"extract_#{type}_dimensions" : nil
+      end
+    end
+
+    def extract_gif_dimensions
+      if @size >= 10
+        @width, @height = @data.unpack("x6v2")
+        process nil
+      end
+    end
+
+    def extract_png_dimensions
+      if @size >= 24
+        @width, @height = @data.unpack("x16N2")
+        process nil
+      end
+    end
+
+    def extract_jpeg_dimensions
+      scanner = JpegScanner.new(@data)
+      if scanner.scan
+        @width  = scanner.width
+        @height = scanner.height
+        @angle  = scanner.angle
+
+        if @angle == 90 || @angle == 270
+          @width, @height = @height, @width
+        end
+
+        process nil
+      end
+    rescue JpegScanner::ScanError
+    end
+
+    def extract_tiff_dimensions
+      scanner = TiffScanner.new(@data)
+      if scanner.scan
+        @width  = scanner.width
+        @height = scanner.height
+        process nil
+      end
+    rescue TiffScanner::ScanError
+    end
+
+    def match_header(header, bytes)
+      bytes[0, header.length] == header
+    end
+  end
+end
+
+module Dimensions
+  module IO
+    def self.extended(io)
+      io.instance_variable_set(:@reader, Reader.new)
+    end
+
+    def read(*args)
+      super.tap do |data|
+        @reader << data if data
+      end
+    end
+
+    def dimensions
+      [width, height] if width && height
+    end
+
+    def width
+      peek
+      @reader.width
+    end
+
+    def height
+      peek
+      @reader.height
+    end
+
+    def angle
+      peek
+      @reader.angle
+    end
+
+    private
+      def peek
+        unless no_peeking?
+          read(pos + 1024) while @reader.width.nil? && pos < 6144
+          rewind
+        end
+      end
+
+      def no_peeking?
+        @reader.width || closed? || pos != 0
+      end
+  end
+end
+
+module Dimensions
+  class TiffScanner < Scanner
+    include TiffScanning
+
+    WIDTH_TAG  = 0x100
+    HEIGHT_TAG = 0x101
+
+    attr_reader :width, :height
+
+    def initialize(data)
+      @width  = nil
+      @height = nil
+      super
+    end
+
+    def scan
+      scan_header
+
+      scan_ifd do |tag|
+        if tag == WIDTH_TAG
+          @width = read_integer_value
+        elsif tag == HEIGHT_TAG
+          @height = read_integer_value
+        end
+      end
+
+      @width && @height
+    end
+  end
+end
+
+module Dimensions
+  VERSION = "1.3.0"
+end
+
+# Extends an IO object with the `Dimensions::IO` module, which adds
+# `dimensions`, `width`, `height` and `angle` methods. The methods
+# will return non-nil values once the IO has been sufficiently read,
+# assuming its contents are an image.
+def Dimensions(io)
+  io.extend(Dimensions::IO)
+end
+
+module Dimensions
+  class << self
+    # Returns an array of [width, height] representing the dimensions
+    # of the image at the given path.
+    def dimensions(path)
+      io_for(path).dimensions
+    end
+
+    # Returns the width of the image at the given path.
+    def width(path)
+      io_for(path).width
+    end
+
+    # Returns the height of the image at the given path.
+    def height(path)
+      io_for(path).height
+    end
+
+    # Returns the rotation angle of the JPEG image at the given
+    # path. If the JPEG is rotated 90 or 270 degrees (as is often the
+    # case with photos from smartphones, for example) its width and
+    # height will be swapped to accurately reflect the rotation.
+    def angle(path)
+      io_for(path).angle
+    end
+
+    private
+      def io_for(path)
+        Dimensions(File.open(path, "rb")).tap do |io|
+          io.read
+          io.close
+        end
+      end
+  end
+end
